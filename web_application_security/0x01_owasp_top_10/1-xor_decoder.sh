8dcb91ee2bfadca2d3d4326a21fa36148c4a6f32
root <manuelantognazza@gmail.com>
2025-01-16 14:56:53 -0300

    ignored null byte in input

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -8,4 +8,4 @@ for i in $(seq 0 $((${#password}-1))); do
     passwordCrack="$passwordCrack$(printf \\$(printf '%03o' $(($(printf '%d' "'${password:$i:1}") ^ 95))))"
 done
 
-echo $passwordCrack
+echo -e "$passwordCrack"
