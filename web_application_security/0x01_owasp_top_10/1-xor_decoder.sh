138a1bd71420d7a4c14fdedc374627a6e2aae204
root <manuelantognazza@gmail.com>
2025-01-16 14:54:42 -0300

    parece que anda

---
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+password=$(echo "${1#'{xor}'}" | base64 -d)
+
+passwordCrack=""
+
+for i in $(seq 0 $((${#password}-1))); do
+    passwordCrack="$passwordCrack$(printf \\$(printf '%03o' $(($(printf '%d' "'${password:$i:1}") ^ 95))))"
+done
+
+echo $passwordCrack
