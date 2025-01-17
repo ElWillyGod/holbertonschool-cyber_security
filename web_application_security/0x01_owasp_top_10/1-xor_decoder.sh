d8e7fb0ad7ddfade0f75cdf34c98e7a149da1da7
root <manuelantognazza@gmail.com>
2025-01-17 16:59:16 -0300

    vuln

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-password=$(echo "${1#'{xor}'}" | base64 -d | tr -d '\0')
-
-passwordCrack=""
-
-for ((i=0; i< ${#password}; i++)); do 
-	passwordCrack="$passwordCrack$(printf '\\x%x' $(printf $(($(printf '%d' "'${password:$i:1}") ^ 95))))"
-done
-
-echo -e "$passwordCrack"
+python3 -c "from base64 import b64decode; print(bytes(byte ^ 0x5f for byte in b64decode('$1'.replace('{xor}', ''))).decode('utf-8'))"
