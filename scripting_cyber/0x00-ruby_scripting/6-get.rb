828925006e61e0aa4580cd64850a208559f46824
ElWillyGod <manuelantognazza@gmail.com>
2025-05-07 17:51:47 -0300

    get recuest

---
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -0,0 +1,14 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+def get_request(url)
+  uri = URI.parse(url)
+  response = Net::HTTP::get_response(uri)
+
+  body = JSON.pretty_generate(JSON.parse(response.body))
+
+  puts "Response code: #{response.code} #{response.message}"
+
+  puts "Response body: \n#{body}"
+end
