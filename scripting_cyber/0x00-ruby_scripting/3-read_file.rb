76d913c16f6b79f608e81e0b3c7307a1da859490
root <manuelantognazza@gmail.com>
2025-05-07 10:38:01 -0300

    read file

--- scripting_cyber/0x00-ruby_scripting/3-read_file.rb
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -1,1 +1,20 @@
+require 'json'
+
 def count_user_ids(path)
+
+  file_content = File.read(path)
+  data = JSON.parse(file_content)
+
+
+  user_ids = Hash.new(0)
+
+  data.each do |item|
+    user_id = item['userId']
+    user_ids[user_id] += 1
+  end
+
+  user_ids.each do |user_id, count|
+    puts "#{user_id}: #{count}"
+  end
+
+end
