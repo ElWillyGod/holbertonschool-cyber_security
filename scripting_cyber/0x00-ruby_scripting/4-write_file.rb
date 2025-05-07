4b7e974e15adb849271dbb175a9517c94e29407b
root <manuelantognazza@gmail.com>
2025-05-07 11:29:00 -0300

    write file

---
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -0,0 +1,18 @@
+require 'json'
+
+def merge_json_files(file1_path, file2_path)
+
+  file1 = File.read(file1_path)
+  data1 = JSON.parse(file1)
+
+
+  file2 = File.read(file2_path)
+  data2 = JSON.parse(file2)
+
+  data = data1 + data2
+
+  File.write(file2_path, JSON.pretty_generate(data))
+
+  puts "#{file2_path}"
+
+end
